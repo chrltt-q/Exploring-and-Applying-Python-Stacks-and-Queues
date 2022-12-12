f1f7022abde3e6b7027119469c798de51ec1dd83
[chrltt-q] <[charlottequezada17@gmail.com]>
2022-12-12 22:22:20 +0800

    def enqueue

--- Application_of_Different_Queues.py
+++ Application_of_Different_Queues.py
@@ -147,4 +147,8 @@ class PriorityQueue:
     def __init__(self):
         self._elements = []
 
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (priority, value))
 
+    def dequeue(self):
+        return heappop(self._elements)
