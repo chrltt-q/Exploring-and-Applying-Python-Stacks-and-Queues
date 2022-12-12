6aca17e19ec1adcdf28e7f179f6f1ed26764bebb
[chrltt-q] <[charlottequezada17@gmail.com]>
2022-12-12 20:00:40 +0800

    print

--- Application_of_Different_Queues.py
+++ Application_of_Different_Queues.py
@@ -26,30 +26,32 @@ class Queue:
 
 # Test the class Queue
 fifo1 = Queue()
-fifo1.enqueue("1st")
-fifo1.enqueue("2nd")
-fifo1.enqueue("3rd")
-
+fifo1.enqueue("1st element")
+fifo1.enqueue("2nd element")
+fifo1.enqueue("3rd element")
+print("This is the first test of FIFO Queue")
 print(fifo1.dequeue())
 print(fifo1.dequeue())
 print(fifo1.dequeue())
 
 
-fifo2 = Queue("1st", "2nd", "3rd")
-print(len(fifo2))
+fifo2 = Queue("1st element", "2nd element", "3rd element")
+print("\nThis is the second test of FIFO Queue")
+print("Length or Number of Elements:", len(fifo2))
 
 for element in fifo2:
-    print(element) # This will print every element inside the variable fifo
+    print(element)  # This will print every element inside the variable fifo2
 
-print(len(fifo2)) # This will display 0 since the elements in the loop were consumed
+print("Length or Number of Elements:", len(fifo2))  # This will display 0 since the elements in the loop were consumed
 
 # Apply the class Queue with input from user
+print("\nThis is the third test of FIFO Queue")
 input1 = input("Please enter the first element you want to add: ")
 input2 = input("Please enter the second element you want to add: ")
 input3 = input("Please enter the third element you want to add: ")
 
 fifo3 = Queue(input1, input2, input3)
-print(len(fifo3))
+print("Length or Number of Elements:", len(fifo3))
 num_input = 1
 while True:
     for element in fifo3:
@@ -57,4 +59,4 @@ while True:
         num_input = num_input + 1
     break
 
-print(len(fifo3))
+print("Length or Number of Elements:", len(fifo3))
