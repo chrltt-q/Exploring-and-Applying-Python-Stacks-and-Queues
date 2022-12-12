96855b36ae2f065c8938752ec06039025e090dbb
[chrltt-q] <[charlottequezada17@gmail.com]>
2022-12-12 17:14:35 +0800

    import deque

--- Application of Different Queues.py
+++ Application of Different Queues.py
@@ -3,3 +3,8 @@ print("****** Charlotte Quezada ******")
 print("********** BSCOE 2-2 **********")
 print("*** Sir Danilo Madrigalejos ***\n")
 
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self._elements = deque()
