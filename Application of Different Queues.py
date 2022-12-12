983dba291f036c2cbd11e60aa870fb926303e147
[chrltt-q] <[charlottequezada17@gmail.com]>
2022-12-12 17:21:26 +0800

    self, *elements

--- Application of Different Queues.py
+++ Application of Different Queues.py
@@ -6,8 +6,8 @@ print("*** Sir Danilo Madrigalejos ***\n")
 from collections import deque
 
 class Queue:
-    def __init__(self):
-        self._elements = deque()
+    def __init__(self, *elements):
+        self._elements = deque(elements)
 
     def enqueue(self, element):
         self._elements.append(element)
