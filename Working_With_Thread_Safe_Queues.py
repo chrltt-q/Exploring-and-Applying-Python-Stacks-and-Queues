41853e83a3a77b8dd83fbb83ee9a6e2528ab734d
[chrltt-q] <[charlottequezada17@gmail.com]>
2022-12-13 16:50:56 +0800

    class View

--- Working_With_Thread_Safe_Queues.py
+++ Working_With_Thread_Safe_Queues.py
@@ -136,3 +136,43 @@ class Consumer(Worker):
             self.buffer.task_done()
             self.simulate_idle()
 
+
+class View:
+    def __init__(self, buffer, producers, consumers):
+        self.buffer = buffer
+        self.producers = producers
+        self.consumers = consumers
+
+    def animate(self):
+        with Live(
+            self.render(), screen=True, refresh_per_second=10
+        ) as live:
+            while True:
+                live.update(self.render())
+
+    def render(self):
+
+        match self.buffer:
+            case PriorityQueue():
+                title = "Priority Queue"
+                products = map(str, reversed(list(self.buffer.queue)))
+            case LifoQueue():
+                title = "Stack"
+                products = list(self.buffer.queue)
+            case Queue():
+                title = "Queue"
+                products = reversed(list(self.buffer.queue))
+            case _:
+                title = products = ""
+
+        rows = [
+            Panel(f"[bold]{title}:[/] {', '.join(products)}", width=82)
+        ]
+        pairs = zip_longest(self.producers, self.consumers)
+        for i, (producer, consumer) in enumerate(pairs, 1):
+            left_panel = self.panel(producer, f"Producer {i}")
+            right_panel = self.panel(consumer, f"Consumer {i}")
+            rows.append(Columns([left_panel, right_panel], width=40))
+        return Group(*rows)
+
+
